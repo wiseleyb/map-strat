5cad39992c1c2901b51960f20af52be0d701eb95
Ben Wiseley <wiseleyb@gmail.com>
2023-09-16 12:33:49 -0700

    Clean up map code

--- app/map.rb
+++ app/map.rb
@@ -1,4 +1,31 @@
 module Map
+  # draws the map
+  # Currently just reads MAP_FILE and draws that based on
+  #   w: water
+  #   m: mountain
+  #   .: land
+  # Grid is layed out like DragonRuby so 0,0 is bottom left (so typical
+  # math layout versus typical programming grid)
+  def map_draw(args)
+    args.state.map_txt ||= args.gtk.read_file(MAP_FILE)
+    args.state.tiles ||=
+      Array.new(MAX_X / TILE_SIZE) { Array.new(MAX_Y / TILE_SIZE, {}) }
+
+    #x = 0
+    #y = MAX_Y - TILE_SIZE
+    # lines = y axis
+    # colums = x axis
+    args.state.map_txt.split("\n").reverse.each_with_index do |line, idy|
+      puts "#{line} :: #{idy} #{line.chars}" if idy == 0
+      line.chars.each_with_index do |char, idx|
+        puts "#{char} #{idx}, #{idy}" if idy == 0
+        args.state.tiles[idx][idy][:tile] = char
+        map_draw_tile(args, idx, idy)
+      end
+    end
+  end
+
+  # draws a map tile based on data in args.state.tiles
   def map_draw_tile(args, xgrid, ygrid)
     px = xgrid * TILE_SIZE
     #py = MAX_Y - ((ygrid + 1) * TILE_SIZE)
@@ -31,35 +58,24 @@ module Map
     map_draw_border(args, xgrid, ygrid, tile)
   end
 
-  # px, py: grid coords, not pixel coords
-  def map_draw_border(args, px, py, tile)
+  # draws border around tile
+  # if tiles is selected it's marked with a different color
+  # colors from https://colorbrewer2.org/#type=sequential&scheme=BuGn&n=3
+  def map_draw_border(args, xgrid, ygrid, tile)
     border = tile.dup
-    clr = args.state.map_selected == [px, py] ? 200 : 0
-    border[:r] = clr
-    border[:g] = clr
-    border[:b] = clr
+    if args.state.map_selected == [xgrid, ygrid]
+      border[:r] = 240
+      border[:g] = 240
+      border[:b] = 240
+    else
+      border[:r] = 0
+      border[:g] = 0
+      border[:b] = 0
+    end
     args.outputs.borders << border
   end
 
-  def map_draw(args)
-    args.state.map_txt ||= args.gtk.read_file(MAP_FILE)
-    args.state.tiles ||=
-      Array.new(MAX_X / TILE_SIZE) { Array.new(MAX_Y / TILE_SIZE, {}) }
-
-    #x = 0
-    #y = MAX_Y - TILE_SIZE
-    # lines = y axis
-    # colums = x axis
-    args.state.map_txt.split("\n").reverse.each_with_index do |line, idy|
-      puts "#{line} :: #{idy} #{line.chars}" if idy == 0
-      line.chars.each_with_index do |char, idx|
-        puts "#{char} #{idx}, #{idy}" if idy == 0
-        args.state.tiles[idx][idy][:tile] = char
-        map_draw_tile(args, idx, idy)
-      end
-    end
-  end
-
+  # marks tile selected if user mouse clicks on it
   def map_set_tile_select(args)
     return unless args.inputs.mouse.button_left
     x, y = [args.inputs.mouse.x, args.inputs.mouse.y]
